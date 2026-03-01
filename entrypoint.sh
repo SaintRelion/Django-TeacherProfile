209dd01e19b24c9cdbcf7fe1541b299d3b4935de
SaintRelion <saintrelion@gmail.com>
2026-03-02 03:23:50 +0800

    removed trailing &

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ echo "Running migrations..."
 python manage.py migrate --noinput
 
 echo "Starting Uvicorn..."
-uvicorn core.asgi:application --host 0.0.0.0 --port 8100 &
+uvicorn core.asgi:application --host 0.0.0.0 --port 8100
