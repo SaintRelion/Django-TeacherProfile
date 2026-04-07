2f14f7894ea30dafd1d4bdcafe7aa48acf4bd47e
SaintRelion <saintrelion@gmail.com>
2026-04-07 19:14:42 +0800

    fixed teacher

--- accounts/serializer.py
+++ accounts/serializer.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers
+from rest_framework import serializers, exceptions
 
 from django.contrib.auth.models import Group
 from django.contrib.auth import get_user_model
@@ -15,25 +15,44 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         fields = "__all__"
         read_only_fields = ["id"]
 
-    def create(self, validated_data):
-        roles = validated_data.pop("roles", [])
-        password = validated_data.pop("password")
+    def validate(self, attrs: dict) -> dict:
+        username: str = attrs.get("username", "")
+        email: str = attrs.get("email", "")
+        target_username: str = username if username else email
 
-        # Set username = email if not provided
-        if "username" not in validated_data or not validated_data["username"]:
-            validated_data["username"] = validated_data.get("email", None)
-            if validated_data["username"] is None:
-                raise serializers.ValidationError("Email is required for username.")
+        if User.objects.filter(username=target_username).exists():
+            # We use PermissionDenied or a custom Exception to bypass DRF's
+            # automatic list-wrapping of ValidationErrors.
+            # This ensures the output is {"detail": "message"} NOT {"detail": ["message"]}
+            exc = exceptions.APIException("A user with this username already exists.")
+            exc.status_code = 400
+            raise exc
 
-        user = User.objects.create(**validated_data)
-        user.set_password(password)
-        user.save()
+        return attrs
 
-        for role_name in roles:
-            group, _ = Group.objects.get_or_create(name=role_name)
-            user.groups.add(group)
+    def create(self, validated_data: dict):
+        roles: list[str] = validated_data.pop("roles", [])
+        password: str = validated_data.pop("password")
 
-        return user
+        # Username logic (already validated in validate() above)
+        if not validated_data.get("username"):
+            validated_data["username"] = validated_data.get("email")
+
+        try:
+            # create_user handles password hashing and internal user logic
+            user = User.objects.create_user(password=password, **validated_data)
+
+            for role_name in roles:
+                group, _ = Group.objects.get_or_create(name=role_name)
+                user.groups.add(group)
+
+            return user
+
+        except Exception as e:
+            # Force the exception message to a string to satisfy 'handleFetch'
+            exc = exceptions.APIException(str(e))
+            exc.status_code = 400
+            raise exc
 
 
 class MeSerializer(serializers.ModelSerializer):
