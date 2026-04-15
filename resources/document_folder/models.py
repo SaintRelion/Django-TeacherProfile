6d14eab44f4722d35b690cd167aea5976ce9d9d9
SaintRelion <saintrelion@gmail.com>
2026-04-15 18:19:16 +0800

    has expirry folder

--- resources/document_folder/models.py
+++ resources/document_folder/models.py
@@ -5,6 +5,7 @@ from sr_libs.dal.mixins import ArchiveMixin
 class DocumentFolder(ArchiveMixin):
     name = models.CharField(max_length=255)
     user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
+    has_expiry = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
